c7a99719619d02b5134f18ffa7b4963d71bd5689
Jaied Al Sabid <30983626+jaiedsabid@users.noreply.github.com>
2022-06-05 19:30:50 +0600

    Added Image Slider

--- part-2/src/App.jsx
+++ part-2/src/App.jsx
@@ -1,10 +1,12 @@
 import Footer from './components/Footer';
 import NavBar from './components/NavBar';
+import Slider from './components/Slider';
 
 function App() {
     return (
         <div className="App">
             <NavBar />
+            <Slider />
             <Footer />
         </div>
     );
---
+++ part-2/src/components/Slider.jsx
@@ -0,0 +1,60 @@
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// import required modules
+import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper';
+
+export default function Slider() {
+    const images = [
+        'image_1.jpg',
+        'image_2.jpg',
+        'image_3.jpg',
+        'image_4.jpg',
+        'image_5.jpg',
+        'image_6.jpg',
+        'image_7.jpg',
+    ];
+
+    return (
+        <>
+            <Swiper
+                cssMode={true}
+                navigation={true}
+                pagination={true}
+                mousewheel={true}
+                keyboard={true}
+                loop={true}
+                modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+                className="relative"
+            >
+                {images.map((image, index) => (
+                    <SwiperSlide key={index}>
+                        <div className="absolute bg-gray-900 inset-0 opacity-50"></div>
+                        <img
+                            className="max-h-screen w-full object-cover"
+                            src={`/images/${image}`}
+                            alt="slider"
+                        />
+                        <div className="absolute drop-shadow-md top-1/2 left-1/2 -translate-x-1/2 translate-y-[-50%] mx-auto">
+                            <h1 className="text-4xl font-extrabold tracking-tight text-white sm:text-5xl lg:text-6xl">
+                                Best Place to find your dream home
+                            </h1>
+                            <p className="mt-6 text-xl text-indigo-100 max-w-3xl">
+                                Places that make you happy and make you feel
+                                good about yourself. Travel is a great way to
+                                experience new cultures, new people, new places,
+                                and new experiences.
+                            </p>
+                        </div>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </>
+    );
+}
